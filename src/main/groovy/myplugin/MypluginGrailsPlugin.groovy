571f2a02319b31dd66e00bad1c88b6e99bef2d8a
Scott Murphy <scott@alwaysvip.com>
2021-09-04 10:43:54 -0700

    5.0.0.RC1

--- src/main/groovy/myplugin/MypluginGrailsPlugin.groovy
+++ src/main/groovy/myplugin/MypluginGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.plugins.*
 class MypluginGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "4.0.12 > *"
+    def grailsVersion = "5.0.0.RC1 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
